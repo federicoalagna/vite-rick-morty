318e2516ed270bd3a8cd5ab8a732d4191f27aedd
federicoalagna <federicoalagna01@gmail.com>
2024-06-03 16:50:37 +0200

    server e chiamata axios

--- vite-rick-morty/src/App.vue
+++ vite-rick-morty/src/App.vue
@@ -1,10 +1,33 @@
-<script setup>
+<script>
+import axios from 'axios';
 import Card from './components/Card.vue'
+import MainComponent from './components/Main.vue'
+export default {
+  components: {
+    CharacterCard
+  },
+  data() {
+    return {
+      characters: [] 
+    };
+  },
+  mounted() {
+    axios.get('https://rickandmortyapi.com/api/character')
+      .then(response => {
+        this.characters = response.data.results; 
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }
+}
 </script>
 
 <template>
-
+  <div>
+    <CharacterCard :characters="characters" />
+  </div>
 </template>
 
-<style scoped>
+<style>
 </style>
---
+++ vite-rick-morty/src/components/Main.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+    name: 'MainComponent'
+}
+</script>
+<template>
+    <div class="container">
+        <div>ciao</div>
+    </div>
+</template>
+<style>
+</style>
